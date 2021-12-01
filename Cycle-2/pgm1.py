7e9d69acac3a3af5cda6945d43c1f6e5a7bb42e5
nikhilshinukumar <74667418+nikhilshinukumar@users.noreply.github.com>
2021-12-01 11:35:07 +0530

    Add files via upload

---
+++ Cycle-2/pgm1.py
@@ -0,0 +1,7 @@
+#1. Create a three dimensional array specifying float data type and print it.
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]],dtype='f')
+print(arr)
+print(arr.dtype)
